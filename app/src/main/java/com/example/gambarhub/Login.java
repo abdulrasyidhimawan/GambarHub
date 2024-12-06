b055e8d8478f3b4ac65a15d6e0438ef0484aff22
Muhammad Hilmi Adzkia <103296966+MHilmiAdz@users.noreply.github.com>
2024-12-06 21:07:49 +0700

    Summary
    Add :
    - Declare and Assign XML element pada class file
    - onClickListener on register link dan login link
    
    Fix :
    - Memperbaiki constrain pada logo google dan logo app.

--- app/src/main/java/com/example/gambarhub/Login.java
+++ app/src/main/java/com/example/gambarhub/Login.java
@@ -1,6 +1,12 @@
 package com.example.gambarhub;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +15,39 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Login extends AppCompatActivity {
+    private EditText emailinput;
+    private EditText passwordinput;
+    private Button loginButton;
+    private TextView daftarlink;
+    private ImageView logingoogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        emailinput = findViewById(R.id.masuk_kolom_email);
+        passwordinput = findViewById(R.id.masuk_kolom_password);
+        loginButton = findViewById(R.id.masuk_tombol);
+        daftarlink = findViewById(R.id.targetDaftar);
+        logingoogle = findViewById(R.id.logo1);
+
+        daftarlink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+            }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
     }
 }
--- app/src/main/java/com/example/gambarhub/Register.java
+++ app/src/main/java/com/example/gambarhub/Register.java
@@ -1,6 +1,12 @@
 package com.example.gambarhub;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +16,34 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Register extends AppCompatActivity {
 
+    private EditText emailinput;
+    private EditText passwordinput;
+    private EditText namainput;
+    private Button regButton;
+    private TextView loginlink;
+    private ImageView logingoogle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
+
+        emailinput = findViewById(R.id.daftar_kolom_email);
+        namainput = findViewById(R.id.daftar_kolom_nama);
+        passwordinput = findViewById(R.id.daftar_kolom_password);
+        regButton = findViewById(R.id.daftar_tombol);
+        loginlink = findViewById(R.id.targetDaftar);
+        logingoogle = findViewById(R.id.logo1);
+
+        loginlink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Register.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
